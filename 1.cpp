9af89c3da11c0299d929e6fe539449dd6849b3dd
shreyansh-error-89 <shreyanshrathore75649@gmail.com>
2023-04-11 21:18:51 +0530

    pointer

---
+++ 1.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+    // pointers
+
+    int a = 20;
+    // int *ptr ;
+    // ptr = &a;
+    // cout<<ptr<<endl;
+    // cout<<*ptr<<endl;
+
+
+    // int char symmetry
+    // char d = 'a';
+    // int *pointer = &c;  These is wrong
+    // char *pointer = &d;
+    // cout<<pointer<<endl;
+    // cout<<*pointer<<endl;
+
+
+    // they all are right
+    // int *p;
+    // int* p;
+    // int*p;
+
+
+
+    // DOUBLE POINTERS
+
+    // int **p;
+    // int *p1 = &a;
+    // // p = &a;    IT WILL GIVE ERROR CAUSE IT IS HERE FOR ONLY STORING THE AFRESS OF A POINTER
+
+    // p = &p1;
+
+    // cout<<p1<<endl;
+    // cout<<*p1<<endl;
+    // cout<<p<<endl;
+    // cout<<*p<<endl;
+    // cout<<**p<<endl;
+
+    int x[] = {1,2,3,4};
+    int *ptr ;
+    ptr = x;
+    int *ptr1 = &x[0];
+    cout<<ptr<<" "<<ptr1;
+
+
+
+
+    
+
+
+    return 0;
+}
